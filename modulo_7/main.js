7561e40cf2d7dbd057b671b6b9acd2f56730de83
Jonas Araujo <eujonas.a@outlook.com>
2023-04-21 20:13:31 -0300

    Ajustes Index e Css / Criação Main.js

---
+++ modulo_7/main.js
@@ -0,0 +1,60 @@
+
+let formStatus = false;
+
+const form = document.getElementById('form-validacao');
+console.log(form);
+
+
+    const numeroA = document.getElementById('numero-a');
+    const numeroB = document.getElementById('numero-b');
+    
+    const notifMsgOK = document.querySelector('.success-message');
+
+function validarNum() {
+    const A = numeroA.value
+    const B = numeroB.value
+
+    return B > A
+}
+
+numeroA.addEventListener('keyup', function (e) {
+    console.log(e.target.value);
+    notifMsgOK.style.display = 'none';
+
+})
+
+
+form.addEventListener('submit', function (e) {
+    e.preventDefault();
+
+    const mensagemOK = `<b>Validação feita</b>! O valor <b>${numeroA.value}</b> é maior que o número <b>${numeroB.value}</b>.`
+
+
+    formStatus = validarNum();
+    if (formStatus) {
+        
+        notifMsgOK.innerHTML = mensagemOK;
+        notifMsgOK.style.display = 'block';
+
+        numeroB.classList.remove('error');
+        document.querySelector('.error-message').style.display = 'none';
+
+        numeroA.value = '';
+        numeroB.value = '';
+
+
+
+    } else {
+
+        if (!formStatus) {
+            numeroB.classList.add('error');
+            document.querySelector('.error-message').style.display = 'block';
+        }
+    }
+
+})
+
+
+
+
+
